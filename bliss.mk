c66f0d63be83f4df9e3552e63322118cab185178
Jack <jackeagle102@gmail.com>
2014-10-15 16:42:17 +0530

    Blissfy

--- bliss.mk
+++ bliss.mk
@@ -1,22 +1,22 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/bliss/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e980/e980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e980
-PRODUCT_NAME := beanstalk_e980
+PRODUCT_NAME := bliss_e980
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-E980
 PRODUCT_MANUFACTURER := LGE
